5614b205420bcce23bf1d146ec3c25d0b9047617
monidresses <monidress.garments@gmail.com>
2026-08-11 18:30:19 +0530

    feat: add Admin runtime and subdomain configuration

---
+++ app-config.js
@@ -0,0 +1,20 @@
+// Moni Dresses Admin runtime configuration.
+export const APP_CONFIG = Object.freeze({
+  app: 'admin',
+  domains: {
+    b2c: 'https://shop.monidresses.com',
+    admin: 'https://admin.monidresses.com',
+    branch: 'https://branch.monidresses.com',
+    wholesale: 'https://wholesale.monidresses.com',
+    creator: 'https://creator.monidresses.com',
+    team: 'https://team.monidresses.com'
+  },
+  firebase: {
+    apiKey: 'AIzaSyBZdG23Io-oMElZ5XVmhVLd87-sq136dhY',
+    authDomain: 'moni-dresses-db.firebaseapp.com',
+    projectId: 'moni-dresses-db',
+    storageBucket: 'moni-dresses-db.firebasestorage.app',
+    messagingSenderId: '24076547918',
+    appId: '1:24076547918:web:6cae50157f9c6749ff501f'
+  }
+});
